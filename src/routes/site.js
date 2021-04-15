7503c45a1dfd093ee28a36f3d2d8a70e0f8bc798
NguyenPhanNhatTu <18110232@student.hcmute.edu.vn>
2021-04-16 02:52:51 +0700

    [Tu:fix] icon name

--- src/routes/site.js
+++ src/routes/site.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const imageController = require('../app/Controllers/ImageController');
 // router.post('/store',imageController.store);
-router.use('/', imageController.index);
+router.use('', imageController.index);
 
 module.exports = router;
